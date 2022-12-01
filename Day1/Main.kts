e262e9ceacfb284a32b5a372918ef9d33443a5ad
Eric Veenendaal <eric@veenendaal.co>
2022-12-01 07:23:05 +0100

    Adding Day1

---
+++ Day1/Main.kts
@@ -0,0 +1,29 @@
+import java.io.File
+
+val inputStream = File("input.txt").inputStream()
+
+val inputString = inputStream.bufferedReader().use { it.readText() }
+
+data class Elf(val number: Int, val total: Int)
+
+var elfNumber = 1
+var total = 0
+var results = listOf<Elf>()
+inputString.split("\\R".toRegex()).toTypedArray().forEach { line ->
+    if (line.isEmpty()) {
+        results += listOf(Elf(elfNumber, total))
+        elfNumber += 1
+        total = 0
+    } else {
+        total += line.toInt()
+    }
+}
+
+val sortedResults = results.sortedBy { it.total }.reversed()
+var grandTotal = 0
+sortedResults.subList(0,3).forEach {
+    grandTotal += it.total
+}
+
+println(sortedResults.subList(0,3))
+println(grandTotal)
