1668e307aa7abac8005cd291079eed3ecfc48f48
Eric Veenendaal <eveenendaal@bol.com>
2022-12-06 08:02:11 +0100

    Answer to Day 6 part 2

--- Day6/part2.main.kts
+++ Day6/part2.main.kts
@@ -1,7 +1,35 @@
 import java.io.File
 
-val inputRanges = File("input.txt").inputStream()
+File("input.txt").inputStream()
     .bufferedReader().use { it.readText() }
     .split("\\R".toRegex()).toTypedArray()
     .filter { it.isNotEmpty() }
+    .forEach {
+        findFirstMarker(it.toCharArray().toTypedArray().asList())
+    }
+
+fun findFirstMarker(input: List<Char>): Int {
+    var messageBuffer: MutableList<Char> = mutableListOf()
+
+    var letterCount = 0
+    input.forEach {
+        letterCount += 1
+
+        messageBuffer += it
+        if (messageBuffer.size == 15) {
+            messageBuffer = messageBuffer.subList(1, 15)
+        }
+
+        if (messageBuffer.size == 14) {
+            if (messageBuffer.toSet().size == messageBuffer.size) {
+                println("Message Start $messageBuffer at $letterCount")
+                return letterCount
+            }
+        }
+
+    }
+
+    println("Not Message Start Found")
+    return -1
+}
 
