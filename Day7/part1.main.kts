a4fc45be112dcc0d0b4f9d1eef70761522bff299
Eric Veenendaal <eveenendaal@bol.com>
2022-12-07 08:42:58 +0100

    Answer to Day 7 part 1 almost solved

--- Day7/part1.main.kts
+++ Day7/part1.main.kts
@@ -1,12 +1,83 @@
 import java.io.File
 
-val inputRanges = File("test.txt").inputStream()
+val inputRanges = File("input.txt").inputStream()
     .bufferedReader().use { it.readText() }
     .split("\\R".toRegex()).toTypedArray()
     .filter { it.isNotEmpty() }
 
+data class MyFile(val name: String, val size: Int)
+data class MyFolder(
+    val name: String,
+    val parent: MyFolder? = null,
+    var files: List<MyFile> = emptyList(),
+    var folders: List<MyFolder> = emptyList(),
+)
 
-inputRanges.forEach {
-    println(it)
+val rootFolder = MyFolder("root")
+var currentFolder = rootFolder
+
+inputRanges.forEach { nextLine ->
+    if (nextLine.startsWith("$")) { // Command
+        val input = nextLine.substring(2)
+        val inputParts = input.split(" ")
+        when (inputParts[0]) {
+            "cd" -> {
+                val folder = inputParts[1]
+                currentFolder = when (folder) {
+                    "/" -> rootFolder
+                    ".." -> currentFolder.parent!!
+                    else -> currentFolder.folders.first { it.name == folder }
+                }
+            }
+
+            "ls" -> {
+
+            }
+        }
+    } else {
+        val inputParts = nextLine.split(" ")
+        if (inputParts[0] == "dir") {
+            currentFolder.folders += MyFolder(inputParts[1], parent = currentFolder)
+        } else {
+            currentFolder.files += MyFile(inputParts[1], inputParts[0].toInt())
+        }
+    }
 }
+
+data class FolderSummary(val name: String, val size: Int)
+
+var folders = emptyList<FolderSummary>()
+
+fun printfolder(folder: MyFolder, level: Int): Int {
+    val space = " ".repeat(level)
+    val size = folder.files.sumOf { it.size }
+    var total = size
+
+    println("$space${folder.name} ($size)")
+    folder.files.forEach {
+        // println("$space- ${it.name} (${it.size})")
+    }
+
+    folder.folders.forEach {
+        val nextFolderSize = printfolder(it, level + 2)
+        if (nextFolderSize < 100000) {
+            total += nextFolderSize
+        }
+    }
+    folders += FolderSummary(folder.name, total)
+
+    return total
+}
+
+val total = printfolder(rootFolder, 0)
+println("Grant Total: $total")
+
+val subfolders = folders
+    .filter { it.size > 0 }
+    .filter { it.size < 100000 }
+println(subfolders)
+println(subfolders.sumOf { it.size })
+
+
+
 
