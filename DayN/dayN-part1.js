c8f3b7bb44f75876fd9383641146929046aa2239
Eric Veenendaal <eric@veenendaal.co>
2022-12-13 20:15:35 +0100

    Adding more js

--- DayN/dayN-part1.js
+++ DayN/dayN-part1.js
@@ -1,6 +1,6 @@
 let fs = require("fs")
 
-fs.readFile("DayN/dayN-test.txt", "utf8", function (err,data) {
+fs.readFile("dayN-test.txt", "utf8", function (err,data) {
   if (err) {
     return console.log(err);
   }
